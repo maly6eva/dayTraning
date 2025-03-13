7563d3a84694b24c5febdb1da4ed2f055efc7e4a
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2025-03-13 21:19:55 +0300

    first commit

---
+++ start/script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+
+if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(
+    function(position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude, longitude];
+        const map = L.map('map').setView(coords, 10);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        map.on(
+            'click',
+            function(mapEvent) {
+            form.classList.remove('hidden')
+            inputDistance.focus()
+    },
+    function() {
+      alert('Вы не предоставили доступ к своей локации')
+    }
+)
+
+
+
+        console.log(mapEvent)
+        const {lat, lng} = mapEvent.latlng;
+        L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'mark-popup'
+                })
+            )
+            .setPopupContent('Тренировка')
+            .openPopup();
+    })
